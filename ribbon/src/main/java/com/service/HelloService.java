6896767095b21b466103068599863933760cbd90
程方林 <919181296@qq.com>
2019-09-17 13:41:41 +0800

    ribbon

---
+++ ribbon/src/main/java/com/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String getHello(){
+        return restTemplate.getForObject("http://SERVICE-HELLOWORLD/",String.class);
+    }
+}
