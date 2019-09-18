102d1a4af8e4691e141f61ab21bdc3fe99e67fac
程方林 <919181296@qq.com>
2019-09-18 11:02:57 +0800

    ribbon的正确启动  ribbon接口缺少restcontroller注解  client模块导入包不对

--- ribbon/src/main/java/com/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/ribbon/RibbonApplication.java
@@ -1,14 +1,18 @@
 package com.ribbon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class RibbonApplication {
 
     public static void main(String[] args) {
@@ -19,5 +23,13 @@ public class RibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
              return new RestTemplate();
+    }
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping("/")
+    public String hello(){
+        return helloService.getHello();
     }
 }
